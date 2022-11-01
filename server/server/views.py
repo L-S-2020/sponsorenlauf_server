11ffe98c153697e6c49da8ba422e7c7f2137267f
Leonard <l@stegle.eu>
2022-11-01 14:04:20 +0100

    Updated files

--- server/server/views.py
+++ server/server/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+import time
 from .models import Student, Runde, School, key
 from django.utils import timezone, dateformat
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -9,26 +11,29 @@ def scanned(request, code):
     authorization = request.META.get('HTTP_AUTHORIZATION', None)
     if key.objects.get(key=authorization) is not None:
         schüler = Student.objects.get(code=code)
-        schüler.kilometer += 1
-        runde = Runde.objects.create(student=schüler, number=schüler.kilometer, time=timezone.now()-schüler.lastseen())
-        schüler.save()
-        schüler.lastseen = timezone.now()
-        schüler.save()
-        gesamt = School.objects.get()
-        gesamt.kilometer += 1
-        gesamt.save()
-        return JsonResponse({"status": "ok", "kilometer": schüler.kilometer, "name": schüler.name})
+        if time.time()-schüler.lastseen > 135:
+            schüler.kilometer += 1
+            runde = Runde.objects.create(student=schüler, number=schüler.kilometer, time=time.time()-schüler.lastseen)
+            schüler.save()
+            schüler.lastseen = time.time()
+            schüler.save()
+            gesamt = School.objects.get()
+            gesamt.kilometer += 1
+            gesamt.save()
+            return JsonResponse({"status": "ok", "kilometer": schüler.kilometer, "name": schüler.name})
+        else:
+            return JsonResponse({"status": "zu schnell", "name": schüler.name})
     else:
         return JsonResponse({"status": "unauthorized"})
 
 def create(request, name):
     authorization = request.META.get('HTTP_AUTHORIZATION', None)
     if key.objects.get(key=authorization) is not None:
-        random_number = Student.objects.make_random_password(length=6, allowed_chars='1234567890')
+        random_number = User.objects.make_random_password(length=6, allowed_chars='1234567890')
         while Student.objects.filter(code=random_number):
-            random_number = Student.objects.make_random_password(length=6, allowed_chars='1234567890')
+            random_number = User.objects.make_random_password(length=6, allowed_chars='1234567890')
     
-        schüler = Student(code=random_number, name=name,)
+        schüler = Student(code=random_number, name=name, lastseen=time.time())
         schüler.save()
         return JsonResponse({"status": "ok", "code": schüler.code, "name": schüler.name})
     else:
@@ -36,7 +41,8 @@ def create(request, name):
     
 
 def main(request):
-    return render(request, "main.html")
+    print(Student.objects.all().count())
+    return render(request, "index.html", {'schüler': Student.objects.all().count(), 'kilometer': School.objects.get().kilometer})
 
 def stats(request, code):
     schüler = Student.objects.get(code=code)
@@ -55,7 +61,7 @@ def start(request):
         schüler = Student.objects.all()
         for i in schüler:
             i.kilometer = 0
-            i.lastseen = timezone.now()
+            i.lastseen = time.time()
             i.save()
         gesamt = School.objects.get()
         gesamt.kilometer = 0
