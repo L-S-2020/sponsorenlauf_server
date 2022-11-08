351f97b6b9c6ee7fd9453cbae6a9f2179f55559e
Leonard <l@stegle.eu>
2022-11-08 18:13:37 +0100

    Design stats.html

--- sponsorenlauf/settings.py
+++ sponsorenlauf/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)5vkz=iwrft-z5wc4jp%n5x8przon)m#^uf7^xb7wx$hh&l0gz"
+SECRET_KEY = "4a9yh2u3e&o@s-j!$coh)2i4vdqrsz8q!rd#m2n40(v$2jn$72"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
