7ffef5afe1c2c86860d9b318e68805ed28596f5e
Leonard <l@stegle.eu>
2022-11-08 18:33:15 +0100

    Design stats.html

--- sponsorenlauf/settings.py
+++ sponsorenlauf/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 
 )
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
