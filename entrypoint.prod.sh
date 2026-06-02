3127624279b019d86f3e5abdcec1e1a2f6a8fdb9
leost <l@stegle.eu>
2026-06-02 23:23:05 +0200

    createsuperuser in containersetup

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,5 +3,5 @@
 python manage.py migrate --noinput
 python manage.py migrate --noinput --run-syncdb
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --noinput --username leo --email leo@example.com
+python manage.py createsuperuser --noinput
 python -m gunicorn --bind 0.0.0.0:8000 --workers 3 sponsorenlauf.wsgi:application
